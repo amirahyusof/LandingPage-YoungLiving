b03b15b3f0e6fdc95ae56d5104514b4c208e5e5b
Amirah Yusof <mirez.data@gmail.com>
2025-11-07 11:42:03 +0800

    modify main page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,28 @@
 // This is the main page of the application
 import React from 'react'
-import Version3Page from './version3/page'
+import HeroSection from './component/hero'
+import ProductSection from './component/product'
+import BenefitSection from './component/benefits'
+import AboutSection from './component/about'
+import Header from './component/header'
+import Footer from './component/footer'
 
 
 export default function MainPage(){
   return (
     <div className="flex flex-col min-h-screen mx-auto justify-center">
-      <Version3Page />
+      <div className='flex flex-col min-h-screen items-center'>
+        <Header />
+  
+        <main className='flex-1 w-full'>
+        <HeroSection />
+        <ProductSection />
+        <BenefitSection />
+        <AboutSection />
+        </main>
+  
+        <Footer />
+      </div>
     </div>
   )
 }
